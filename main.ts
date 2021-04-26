cbdb515a7a2fac6819cf51e62a260f3bb4381a68
adw2021 <83194876+adw2021@users.noreply.github.com>
2021-04-26 16:29:46 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,42 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showNumber(input.temperature())
+})
+basic.forever(function () {
+    if (input.compassHeading() == 0) {
+        basic.showLeds(`
+            # . . . #
+            # . . . #
+            # . . . #
+            # . . . #
+            . # # # .
+            `)
+    }
+    if (input.compassHeading() == 90) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # # # .
+            . # . . .
+            . # # # .
+            `)
+    }
+    if (input.compassHeading() == 180) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . . # # .
+            . . . # .
+            . # # # .
+            `)
+    }
+    if (input.compassHeading() == 270) {
+        basic.showLeds(`
+            . . . . .
+            # . # . #
+            # . # . #
+            # . # . #
+            . # . # .
+            `)
+    }
+})
